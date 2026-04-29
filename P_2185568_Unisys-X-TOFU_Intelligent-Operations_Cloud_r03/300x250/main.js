99227837264b877c3d32ebe6cbe637dc0a98e083
Mahesh Bhatt <mahesh.bhatt@omc.com>
2026-04-29 23:40:06 +0530

    dfg

--- P_2185568_Unisys-X-TOFU_Intelligent-Operations_Cloud_r03/300x250/main.js
+++ P_2185568_Unisys-X-TOFU_Intelligent-Operations_Cloud_r03/300x250/main.js
@@ -18,31 +18,8 @@ function animate() {
 
 
   tl.from(split.words, 0.5, { y: 15, opacity: 0, filter: "blur(7px)", stagger: 0.2, ease: "power2.out" }, 0.5);
-  // tl.set([logo, img, bg1, bg2, bg3, bg4, copy1, copy2, copy3, copy4, cta], {force3D: true});
-  // tl.set([copy2, copy3, copy4], { xPercent: -120 });
-  // tl.set([bg4], { y: 260 });
-
-  // tl.addLabel("frame1", 1)
-  //   .to(logo, 1, { width: 121, x: 34.5, y: 3, ease: "power1.inOut" }, "frame1")
-  //   .to(img, 1, { width: 275, height: 157, x: 13, y: 11, ease: "power1.inOut" }, "frame1")
-  //   .to(bg1, 1, { width: 306, x: -4, y: -42, ease: "power1.inOut" }, "frame1")
-  //   .from(copy1, 0.5, { xPercent: -120, ease: "power1.inOut" }, "frame1+=1")
-  //   .to(cta, 0.5, { autoAlpha:1, ease: "power1.inOut" }, "frame1+=1.7")
   
-  // .addLabel("frame2", "frame1+=3")
-  //   .to([bg1, copy1], 0.5, {autoAlpha:0, ease: "power1.inOut" }, "frame2")
-  //   .to(bg2, 0.5, {autoAlpha:1, ease: "power1.inOut" }, "frame2")
-  //   .to(copy2, 0.5, { xPercent: 0, ease: "power1.inOut" }, "frame2+=0.5")
-
-  // .addLabel("frame3", "frame2+=3")
-  //   .to([bg2, copy2], 0.5, {autoAlpha:0, ease: "power1.inOut" }, "frame3")
-  //   .to(bg3, 0.5, {autoAlpha:1, ease: "power1.inOut" }, "frame3")
-  //   .to(copy3, 0.5, { xPercent: 0, ease: "power1.inOut" }, "frame3+=0.5")
-
-  // .addLabel("frame4", "frame3+=3")
-  //   .to([copy3, logo], 0.5, {autoAlpha:0, ease: "power1.inOut" }, "frame4")
-  //   .to(bg4, 0.5, { y: 0,ease: "power1.inOut" }, "frame4")
-  //   .to(copy4, 0.5, { xPercent: 0, ease: "power1.inOut" }, "frame4+=0.5")
+  tl.to(".bg", 10, { x:0, ease: "power.inOut" }, 0.5);
 }
 
 function setRollover() {
